9b2da615ef8d2b8af668f6e14d01c9a5b53e353e
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2017-12-12 15:55:09 +0100

    Added helper methods in the `MesosApi` trait to build mesos `Call`s

--- mesos-client/src/main/scala/mesosphere/mesos/client/MesosClient.scala
+++ mesos-client/src/main/scala/mesosphere/mesos/client/MesosClient.scala
@@ -3,19 +3,21 @@ package mesosphere.mesos.client
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers.{ Accept, RawHeader }
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.recordio.scaladsl.RecordIOFraming
 import akka.stream.scaladsl._
 import akka.util.ByteString
-import akka.{ Done, NotUsed }
+import akka.{Done, NotUsed}
 import com.typesafe.scalalogging.StrictLogging
 import mesosphere.mesos.conf.MesosConf
-import org.apache.mesos.v1.Protos.FrameworkInfo
-import play.api.libs.json.{ JsObject, JsValue, Json }
+import org.apache.mesos.v1.mesos._
+import org.apache.mesos.v1.scheduler.scheduler.Call
+import org.apache.mesos.v1.scheduler.scheduler.Call.{Accept, Acknowledge, Decline, Kill, Message, Reconcile, Revive}
+import play.api.libs.json.{JsObject, JsValue, Json}
+import com.google.protobuf
 
-import scala.concurrent.{ ExecutionContext, Future, Promise }
-import scala.util.{ Failure, Success }
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
 
 class MesosClient(
     conf: MesosConf,
@@ -58,7 +60,7 @@ class MesosClient(
       HttpMethods.POST,
       uri = Uri("/api/v1/scheduler"),
       entity = HttpEntity(MediaTypes.`application/json`, body),
-      headers = List(Accept(MediaTypes.`application/json`)))
+      headers = List(headers.Accept(MediaTypes.`application/json`)))
 
     val httpConnection: Flow[HttpRequest, HttpResponse, Future[Http.OutgoingConnection]] = Http().outgoingConnection(host, port.toInt)
 
@@ -120,7 +122,7 @@ class MesosClient(
         HttpMethods.POST,
         uri = Uri(s"http://$host:$port/api/v1/scheduler"),
         entity = HttpEntity(MediaTypes.`application/json`, call),
-        headers = List(Accept(MediaTypes.`application/json`), RawHeader("Mesos-Stream-Id", mesosStreamId)))
+        headers = List(headers.Accept(MediaTypes.`application/json`), headers.RawHeader("Mesos-Stream-Id", mesosStreamId)))
 
       Http().singleRequest(request).map{ response =>
         logger.info(s"Response: $response")
@@ -157,7 +159,6 @@ trait MesosApi {
     * }
     * ```
     *
-    * Mesos documentation on SUBSCRIBE call:
     * http://mesos.apache.org/documentation/latest/scheduler-http-api/#subscribe-1
     */
   def mesosSource: Source[String, NotUsed]
@@ -169,17 +170,201 @@ trait MesosApi {
     * published to sink without a successful subscription will be buffered and will have to wait for subscription
     * connection. Always call `source()` first.
     *
-    * More on mesos `Call`s:
     * http://mesos.apache.org/documentation/latest/scheduler-http-api/#calls
     */
   def mesosSink: Sink[String, NotUsed]
+
+  /** ***************************************************************************
+    * Helper methods to create mesos `Call`s
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#calls
+    * ***************************************************************************
+    */
+
+  /** Sent by the scheduler when it wants to tear itself down. When Mesos receives this request it will
+    * shut down all executors (and consequently kill tasks). It then removes the framework and closes all
+    * open connections from this scheduler to the Master.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#teardown
+    */
+  def teardown(frameworkId: FrameworkID): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.TEARDOWN)
+    )
+  }
+
+  /** Sent by the scheduler when it accepts offer(s) sent by the master. The ACCEPT request includes the type
+    * of operations (e.g., launch task, launch task group, reserve resources, create volumes) that the scheduler
+    * wants to perform on the offers. Note that until the scheduler replies (accepts or declines) to an offer,
+    * the offer’s resources are considered allocated to the offer’s role and to the framework.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#accept
+    */
+  def accept(frameworkId: FrameworkID, accepts: Accept): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.ACCEPT),
+      accept = Some(accepts))
+  }
+
+  /** Sent by the scheduler to explicitly decline offer(s) received. Note that this is same as sending an ACCEPT
+    * call with no operations.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#decline
+    */
+  def decline(frameworkId: FrameworkID, offerIds: Seq[OfferID], filters: Option[Filters] = None): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.DECLINE),
+      decline = Some(Decline(offerIds = offerIds, filters = filters))
+    )
+  }
+
+  /** Sent by the scheduler to remove any/all filters that it has previously set via ACCEPT or DECLINE calls.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#revive
+    */
+  def revive(frameworkId: FrameworkID, roles: Option[String] = None): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.REVIVE),
+      revive = Some(Revive(role = roles))
+    )
+  }
+
+  /** Suppress offers for the specified roles. If `roles` is empty the `SUPPRESS` call will suppress offers for all
+    * of the roles the framework is currently subscribed to.
+    *
+    * http://mesos.apache.org/documentation/latest/upgrades/#1-2-x-revive-suppress
+    */
+  def suppress(frameworkId: FrameworkID, roles: Option[String] = None): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.SUPPRESS),
+      suppress = Some(Call.Suppress(roles))
+    )
+  }
+
+  /** Sent by the scheduler to kill a specific task. If the scheduler has a custom executor, the kill is forwarded
+    * to the executor; it is up to the executor to kill the task and send a TASK_KILLED (or TASK_FAILED) update.
+    * If the task hasn’t yet been delivered to the executor when Mesos master or agent receives the kill request,
+    * a TASK_KILLED is generated and the task launch is not forwarded to the executor. Note that if the task belongs
+    * to a task group, killing of one task results in all tasks in the task group being killed. Mesos releases the
+    * resources for a task once it receives a terminal update for the task. If the task is unknown to the master,
+    * a TASK_LOST will be generated.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#kill
+    */
+  def kill(frameworkId: FrameworkID, taskId: TaskID, agentId: Option[AgentID] = None, killPolicy: Option[KillPolicy]): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.KILL),
+      kill = Some(Kill(taskId = taskId, agentId = agentId, killPolicy = killPolicy))
+    )
+  }
+
+  /** Sent by the scheduler to shutdown a specific custom executor. When an executor gets a shutdown event, it is
+    * expected to kill all its tasks (and send TASK_KILLED updates) and terminate.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#shutdown
+    */
+  def shutdown(frameworkId: FrameworkID, executorId: ExecutorID, agentId: AgentID):Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.SHUTDOWN),
+      shutdown = Some(Call.Shutdown(executorId = executorId, agentId = agentId))
+    )
+  }
+
+  /** Sent by the scheduler to acknowledge a status update. Note that with the new API, schedulers are responsible
+    * for explicitly acknowledging the receipt of status updates that have status.uuid set. These status updates
+    * are retried until they are acknowledged by the scheduler. The scheduler must not acknowledge status updates
+    * that do not have `status.uuid` set, as they are not retried. The `uuid` field contains raw bytes encoded in Base64.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#acknowledge
+    */
+  def acknowledge(frameworkId: FrameworkID, agentId: AgentID, taskId: TaskID, uuid: protobuf.ByteString): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.ACKNOWLEDGE),
+      acknowledge = Some(Acknowledge(agentId, taskId, uuid))
+    )
+  }
+
+  /** Sent by the scheduler to query the status of non-terminal tasks. This causes the master to send back UPDATE
+    * events for each task in the list. Tasks that are no longer known to Mesos will result in TASK_LOST updates.
+    * If the list of tasks is empty, master will send UPDATE events for all currently known tasks of the framework.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#reconcile
+    */
+  def reconcile(frameworkId: FrameworkID, tasks: Seq[Reconcile.Task]): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.RECONCILE),
+      reconcile = Some(Reconcile(tasks))
+    )
+  }
+
+  /** Sent by the scheduler to send arbitrary binary data to the executor. Mesos neither interprets this data nor
+    * makes any guarantees about the delivery of this message to the executor. data is raw bytes encoded in Base64
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#message
+    */
+  def message(frameworkId: FrameworkID, agentId: AgentID, executorId: ExecutorID, message: ByteString): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.MESSAGE),
+      message = Some(Message(agentId, executorId, protobuf.ByteString.copyFrom(message.toByteBuffer)))
+    )
+  }
+
+  /** Sent by the scheduler to request resources from the master/allocator. The built-in hierarchical allocator
+    * simply ignores this request but other allocators (modules) can interpret this in a customizable fashion.
+    *
+    * http://mesos.apache.org/documentation/latest/scheduler-http-api/#request
+    */
+  def request(frameworkId: FrameworkID, requests: Seq[Request]): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.REQUEST),
+      request = Some(Call.Request(requests = requests))
+    )
+  }
+
+  /** Accepts an inverse offer. Inverse offers should only be accepted if the resources in the offer can be safely
+    * evacuated before the provided unavailability.
+    *
+    * https://mesosphere.com/blog/mesos-inverse-offers/
+    */
+  def acceptInverseOffers(frameworkId: FrameworkID, offers: Seq[OfferID], filters: Option[Filters] = None): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.ACCEPT_INVERSE_OFFERS),
+      acceptInverseOffers = Some(Call.AcceptInverseOffers(inverseOfferIds = offers, filters = filters))
+    )
+  }
+
+  /** Declines an inverse offer. Inverse offers should be declined if
+    * the resources in the offer might not be safely evacuated before
+    * the provided unavailability.
+    *
+    * https://mesosphere.com/blog/mesos-inverse-offers/
+    */
+  def declineInverseOffers(frameworkId: FrameworkID, offers: Seq[OfferID], filters: Option[Filters] = None): Call = {
+    Call(
+      frameworkId = Some(frameworkId),
+      `type` = Some(Call.Type.DECLINE_INVERSE_OFFERS),
+      declineInverseOffers = Some(Call.DeclineInverseOffers(inverseOfferIds = offers, filters = filters))
+    )
+  }
+
 }
 
 // TODO: PLAN:
 // TODO: ====================================================================================
 // TODO: Set FrameworkInfo on SUBSCRIBE request
 // TODO: Switch from `application/json` to `application/x-protobuf`
-// TODO: Add helper methods to build mesos `Call`s to the API trait
 // TODO: Add ITs
 // TODO: Add README.md
 
